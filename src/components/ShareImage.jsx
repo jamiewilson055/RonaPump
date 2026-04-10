b2d612c7bcbca43e727f313df6d2b43cff6a5a5f
Jamie <jamie.wilson055@gmail.com>
2026-04-09 20:56:45 -0400

    ShareImage: 4:5 ratio + balanced font tiers (top 2 slightly smaller)

--- src/components/ShareImage.jsx
+++ src/components/ShareImage.jsx
@@ -182,8 +182,8 @@ export default function ShareImage({ workout, onClose }) {
     }
 
     let fontSize, lineH
-    if (effectiveLines <= 4) { fontSize = 64; lineH = 78 }
-    else if (effectiveLines <= 6) { fontSize = 58; lineH = 72 }
+    if (effectiveLines <= 4) { fontSize = 66; lineH = 82 }
+    else if (effectiveLines <= 6) { fontSize = 60; lineH = 74 }
     else if (effectiveLines <= 10) { fontSize = 56; lineH = 70 }
     else if (effectiveLines <= 14) { fontSize = 50; lineH = 64 }
     else if (effectiveLines <= 20) { fontSize = 46; lineH = 58 }
